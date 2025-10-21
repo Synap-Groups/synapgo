eb0e90aa4fb1109e0fac02da4a40d4e256ea1fb4
jzaragosa06 <junjunzaragosa2309@gmail.com>
2025-10-21 23:33:41 +0800

    fix: fix name in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
+import { ENV } from "../configs/env";
+
 export default function Footer() {
     return (
         <footer className="text-center text-gray-600 py-6 border-t border-gray-200">
-            <p>© {new Date().getFullYear()} Chicode. All rights reserved.</p>
+            <p>© {new Date().getFullYear()} {ENV.DEVELOPERS_NAME}. All rights reserved.</p>
         </footer>
     );
 }
